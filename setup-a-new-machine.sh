b9cfc978320cd92cf08a013d99c26ce9cb29a4c9
Wellington Soares <well.cco@gmail.com>
2018-02-23 10:33:10 -0300

    updating the homew-brew installation

--- setup-a-new-machine.sh
+++ setup-a-new-machine.sh
@@ -50,7 +50,7 @@ fi
 ### homebrew!
 
 # (if your machine has /usr/local locked down (like google's), you can do this to place everything in ~/.homebrew
-mkdir $HOME/.homebrew && curl -L https://github.com/mxcl/homebrew/tarball/master | tar xz --strip 1 -C $HOME/.homebrew
+mkdir $HOME/.homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C $HOME/.homebrew
 export PATH=$HOME/.homebrew/bin:$HOME/.homebrew/sbin:$PATH
 
 # install all the things
